a72316898d3698ce901078ab4e3d322482d7341e
Niapoll <Kolya21082001@gmail.com>
2023-12-11 18:28:37 +0400

    Fix tests

--- matrix_algs_test.py
+++ matrix_algs_test.py
@@ -1,5 +1,4 @@
-from matrix_algs import find_independent_zeros
-from models import BipartiteGraph
+from hungarian_method import enumerate_best_assignments_with_prohibitions
 from typing import Sequence
 import numpy as np
 import pytest
@@ -8,38 +7,62 @@ import pytest
 TEST_CASES = [
     (
         # Appointments
-        [[1, 0, 2, 0],
-        [0, 2, 3, 0],
-        [0, 1, 0, 2],
-        [1, 2, 0, 0]],
-
+        [
+            [3, 4, 3, 4, 7, 5],
+            [1, 3, 4, 4, 4, 3],
+            [5, 3, 7, 6, 4, 5],
+            [4, 1, 4, 0, 3, 4],
+            [4, 4, 6, 6, 2, 2],
+            [5, 5, 3, 1, 2, 2],
+        ],
         # Prohibitions
-        [[0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]]
+        [
+            [True, True, True, True, True, True],
+            [True, True, True, True, True, True],
+            [True, True, True, True, True, True],
+            [True, True, True, True, True, True],
+            [True, True, True, True, True, True],
+            [True, True, True, True, True, True]
+        ],
     )
 ]
 
 
 ANSWERS = [
     (
-        # Independent zeros
-        [
-            [(1, 0), (0, 1), (2, 2), (3, 3)],
-            [(1, 3), (0, 1), (2, 0), (3, 2)]
-        ]
+        [(0, 2), (1, 0), (2, 1), (3, 3), (4, 5), (5, 4)],
+        [(0, 2), (1, 0), (2, 1), (3, 3), (4, 4), (5, 5)]
     )
 ]
 
-@pytest.mark.parametrize('appointments,prohibitions,expected', [pytest.param(*(np.array(i) for i in input_data), output_data) for input_data, output_data in zip(TEST_CASES, ANSWERS)])
-def test_check_independent_zeros(appointments: np.ndarray, prohibitions: np.ndarray, expected: Sequence[tuple[int, int]]) -> None:
-    graph = BipartiteGraph(appointments)
 
-    actual = find_independent_zeros(graph)
+@pytest.mark.parametrize(
+    'appointments,prohibitions,expected',
+    [
+        pytest.param(*(np.array(i) for i in input_data), output_data)
+        for input_data, output_data in zip(TEST_CASES, ANSWERS)
+    ]
+)
+def test_solution(
+    appointments: np.ndarray,
+    prohibitions: np.ndarray,
+    expected: Sequence[Sequence[tuple[int, int]]]
+) -> None:
+    actual = [*enumerate_best_assignments_with_prohibitions(appointments, prohibitions)]
+    actual = [sorted((int(k), int(v)) for k, v in a.items()) for a in actual]
 
-    for answer in expected:
-        if answer == actual:
-            return
+    assert len(expected) == len(actual)
 
-    assert False
+    for ex_answer in expected:
+        assert _compare_answers(ex_answer, actual)
+
+
+def _compare_answers(
+    expected: Sequence[tuple[int, int]],
+    actual_answers: Sequence[Sequence[tuple[int, int]]]
+) -> bool:
+    for actual_answer in actual_answers:
+        if actual_answer == expected:
+            return True
+
+    return False
